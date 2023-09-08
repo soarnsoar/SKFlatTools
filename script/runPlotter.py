bf76443cca64d9a6d86c0f1e1e239cbe264d9e69
Junho <soarnsoar@gmail.com>
2023-09-08 18:24:18 +0900

    fix style

--- script/runPlotter.py
+++ script/runPlotter.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python 
+##====================
+##TODO : ratio pad / legend
+##
+##===================
 from Plotter import Plotter
 from glob import glob
 import CMS_lumi, tdrstyle
@@ -20,12 +24,13 @@ class Drawer:
         self.SetNames(title,prefix,variablename)
         self.CombineHist()
         self.SetMinMaxBins()
+        self.GetRatio1Line()
+    
     def CombineHist(self):
         ##---Combine Histos
         self.chdict=OrderedDict()
         for p in self.pdict:
             subidx=0
-
             for subp in self.pdict[p]['procs']: ## subp=TTLL TTLJ will be combined to TTtotal
                 if subidx==0:
                     self.chdict[p]=self.hdict[subp].Clone()
@@ -36,7 +41,6 @@ class Drawer:
         ##---Stack Histos
         mcidx=0
         for p in self.pdict:
-            
             ##--Set data histo
             if 'isData' in self.pdict[p]:
                 if self.pdict[p]['isData']: 
@@ -66,20 +70,12 @@ class Drawer:
     def SetMinMaxBins(self):
         _id=0
         for p in self.chdict:
-            
             if _id==0: 
-                #self.xbins=self.chdict[p].GetXbins()
-                #_mycanv=ROOT.TCanvas()
-                #self.chdict[p].Draw()
-                #_mycanv.SaveAs("temp.pdf")
-                ##--GetXbins() doesn't work. Get bins by hand
                 for i in range(1,self.chdict[p].GetXaxis().GetNbins()+2):
                     _BinLowEdge=self.chdict[p].GetXaxis().GetBinLowEdge(i)
                     self.xbins.append(_BinLowEdge)
-                    
             ymin_=self.chdict[p].GetMinimum()
             ymax_=self.chdict[p].GetMaximum()
-            
             if ymin_<self.ymin:self.ymin=ymin_
             if ymax_>self.ymax:self.ymax=ymax_
             _id+=1
@@ -88,27 +84,36 @@ class Drawer:
             ymax_=_h.GetMaximum()
             if ymin_<self.ymin:self.ymin=ymin_
             if ymax_>self.ymax:self.ymax=ymax_
-
-
-        
         self.xbins=np.array(self.xbins)
+    def GetRatio1Line(self):
+        #TLine (Double_t x1, Double_t y1, Double_t x2, Double_t y2)
+        x1=self.xbins[1]
+        x2=self.xbins[-1]
+        y1=1
+        y2=1
+        self.line1=ROOT.TLine(x1,y1,x2,y2)
+        self.line1.SetLineStyle(2)
     def SetNames(self,title,prefix,xname,yname='Events'):
         self.title=title
         self.xname=xname
         self.yname=yname
         self.prefix=prefix
-    def SetOutputdir(self,outputdir):
-        self.outputdir=outputdir
+    def SetPlotConfig(self,_conf):
+        self.plotconf=_conf
+        self.outputdir=self.plotconf['outputdir']
+        self.lumi=self.plotconf['lumi']
+        self.sqrtS=self.plotconf['sqrtS']
         os.system('mkdir -p '+self.outputdir)
     def run(self):
         ##---------From TDR style ---------##
         tdrstyle.setTDRStyle()
         #change the CMS_lumi variables (see CMS_lumi.py)
-        CMS_lumi.lumi_13TeV = "-1 fb^{-1}"
+        CMS_lumi.lumi_13TeV = self.lumi+" fb^{-1}"
         CMS_lumi.writeExtraText = 1
         CMS_lumi.extraText = "Preliminary"
-        CMS_lumi.lumi_sqrtS = "13 TeV" # used with iPeriod = 0, e.g. for simulation-only plots (default is an empty string)
+        CMS_lumi.lumi_sqrtS = self.sqrtS # used with iPeriod = 0, e.g. for simulation-only plots (default is an empty string)
         iPos = 11
+        CMS_lumi.relPosX    = 0.08
         if( iPos==0 ): CMS_lumi.relPosX = 0.12
         
         H_ref = 600; 
@@ -148,23 +153,13 @@ class Drawer:
         canvas.SetTickx(0)
         canvas.SetTicky(0)
         
-        #h =  ROOT.TH1F("h","h; m_{e^{+}e^{-}} (GeV); Events / 0.5 GeV",80,70,110)
-        #h =  ROOT.TH1F("--","--",len(self.xbins)-1,self.xbins)
-        #xAxis = h.GetXaxis()
-        #xAxis.SetNdivisions(6,5,0)
-        #yAxis = h.GetYaxis()
-        #yAxis.SetNdivisions(6,5,0)
-        #yAxis.SetTitleOffset(1)
-        #h.SetMaximum(self.ymax*1.1)
-        #h.Draw()
+
+        self.hdata.SetMarkerStyle(8)
+        self.hdata.SetMarkerSize(0.5)
         
-        ##----Your Area----##
-
-
-        print type(self.hstack)
         self.hstack.Draw('hist')
-        self.hdata.Draw('ex0sames')
-        ##----[END]Your Area----##
+        self.hdata.Draw('E1sames')
+
         CMS_lumi.CMS_lumi(canvas, iPeriod, iPos)
         canvas.cd()
         canvas.Update()
@@ -172,6 +167,51 @@ class Drawer:
         canvas.SaveAs(self.outputdir+'/'+self.prefix+self.xname+".pdf")
         canvas.SetLogy()
         canvas.SaveAs(self.outputdir+'/'+self.prefix+self.xname+"_log.pdf")
+        canvas.SetLogy(0)
+        ##--ratioplot
+        self.hratio=self.hdata/self.hmc
+        ##
+        pad1=ROOT.TPad("pad1", "pad1", 0, 0.3, 1, 1) ##x1,y1,x2,y2
+        pad1.SetTopMargin(0.1)
+        pad1.SetBottomMargin(0.02)
+        pad1.SetGridx()
+        pad1.Draw()
+        pad1.cd()
+        self.hstack.Draw('hist')
+        self.hdata.Draw('E1sames')
+        
+        canvas.cd()
+        pad2=ROOT.TPad("pad2", "pad2", 0, 0.0, 1, 0.3)
+        pad2.SetTopMargin(0.02)
+        pad2.SetBottomMargin(0.2)
+        pad2.SetGridx()
+        pad2.Draw()
+        pad2.cd()
+        ##TODO::handle this with input args
+        self.hratio.SetMinimum(0.5)        
+        self.hratio.SetMaximum(1.5)
+        self.hratio.GetYaxis().SetLabelSize(0.1)
+        self.hratio.GetXaxis().SetLabelSize(0.1)
+        self.hratio.GetYaxis().SetNdivisions(505)
+        #self.hratio.GetXaxis().SetTitle(self.xtitle)
+        self.hratio.GetXaxis().SetTitleOffset(1)
+        self.hratio.GetXaxis().SetTitleSize(0.09)
+
+        self.hratio.Draw('E1sames')
+        self.line1.Draw('sames')
+        CMS_lumi.CMS_lumi(canvas, iPeriod, iPos)
+        canvas.cd()
+        canvas.Update()
+        canvas.SaveAs(self.outputdir+'/'+self.prefix+self.xname+"_ratio.pdf")
+        pad1.cd()
+        pad1.SetLogy()
+        canvas.cd()
+        canvas.Update()
+        canvas.SaveAs(self.outputdir+'/'+self.prefix+self.xname+"_ratio_log.pdf")
+
+
+
+
         del canvas
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Plot configuration')
@@ -203,7 +243,7 @@ if __name__ == '__main__':
     #flist=glob("/data6/Users/jhchoi/SKFlatOutput/Run2UltraLegacy_v3/MYOUTPUT/test/BasicTest/2017/DATA/*.root")+\
     #glob("/data6/Users/jhchoi/SKFlatOutput/Run2UltraLegacy_v3/MYOUTPUT/test/BasicTest/2017/*.root")
     hdict=p.GetHistFromFileList(plotconf['filelist'])
-    outputdir=plotconf['outputdir']
+    #outputdir=plotconf['outputdir']
     ##----Check Histogram List and Check xmin xmax----#
     xmin=sys.maxsize
     xmax=-sys.maxsize
@@ -212,6 +252,6 @@ if __name__ == '__main__':
             #print c,v
             drawer=Drawer(hdict[c][v],procconf,'',c,v)
             #    def SetNames(self,title,prefix,xname,yname):
-            drawer.SetOutputdir(outputdir)
+            drawer.SetPlotConfig(plotconf)
             drawer.run()
             
