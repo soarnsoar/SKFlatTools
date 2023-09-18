7a3bdaa1707bc03bf8ebc66b9e1bbc67d870b657
Junho <soarnsoar@gmail.com>
2023-09-18 16:35:30 +0900

    1st draft for comparing

---
+++ config/Comparison/StudyBmatjet_gen_lhe/conf_LHE.py
@@ -0,0 +1,13 @@
+dict_conf={
+    "x_b_bbar":{
+        "xname":"x",
+        "yname":"events",
+        "numelist":["gbToZb_b/x_b/DYJets"],## paths of histograms 
+        "color":[2,4],
+        "deno":"gbToZb_b/x_b/DYJets",
+        "setlogx":[True],
+        "setlogy":[True,False],
+        
+    },
+
+}
---
+++ config/Comparison/StudyBmatjet_gen_lhe/run.sh
@@ -0,0 +1,1 @@
+runPlotterComparison.py --plot conf_LHE.py --outputdir plots --inputfile B_Info_Analyzer_DYJets.root
